c7cf4c7d465f2fcd513f5ab5b3677fa5855bf4ab
Musheeb Affan <musheebaffan@Musheebs-MacBook-Air.local>
2026-01-17 14:56:20 +0530

    feat: conditional rendering

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,20 +10,24 @@ function Main({
 }) {
   return (
     <div className="main-card">
-      <img src={image} alt="" />
+      {image && <img src={image} alt="" />}
       <div className="main-card-content">
-        <span className="main-card-location">
-          <i className="fa-solid fa-location-dot main-card-location-icon"></i>
-          <span className="main-card-destination">{country}</span>
-          <span>
-            <a href={locationLink} className="main-card-destination-link">
-              View on Google Maps
-            </a>
+        {country && (
+          <span className="main-card-location">
+            <i className="fa-solid fa-location-dot main-card-location-icon"></i>
+            <span className="main-card-destination">{country}</span>
+            <span>
+              <a href={locationLink} className="main-card-destination-link">
+                View on Google Maps
+              </a>
+            </span>
           </span>
-        </span>
-        <h2 className="main-card-location-name">{locationName}</h2>
-        <span className="main-card-timestamp">{timestamp}</span>
-        <p className="main-card-description">{description}</p>
+        )}
+        {locationName && (
+          <h2 className="main-card-location-name">{locationName}</h2>
+        )}
+        {timestamp && <span className="main-card-timestamp">{timestamp}</span>}
+        {description && <p className="main-card-description">{description}</p>}
       </div>
     </div>
   );
